fe5e60711f62e1b8c657805412a0a60af1c227c2
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-07-29 12:32:46 +0900

    created test card component

---
+++ src/components/ProductCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './productcard.scss'
+
+const ProductCard = () => (
+  <div className="card-box">
+    <div className="card-bg-color-top">
+      <h1 className="card-text-title">Make up Lessons</h1>
+    </div>
+    <img className="card-image" src="https://res.cloudinary.com/dtewjoisz/image/upload/v1595992246/bijin%20carte/pexels-photo-457701_bruobi.webp"></img>
+    <div className="card-bg-color-bottom">
+      <div class="card-text">
+        <p class="text-content">とするのも、こういう自由です勝手も最もばらばらでらく威張っみるなくてなかったです。いくら講義あてるのでもどう自他を活動しれなっやっれるをあるがなりからです。あれは私がたば好きに思うたのを演説思っものだたなく。</p>
+      </div>
+    </div>
+  </div>
+  
+ 
+)
+
+export default ProductCard;
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
+import ProductCard from '../components/ProductCard'
 
 export default ({pageContext}) => (
   <Layout>
     <h1 dangerouslySetInnerHTML={{__html: pageContext.title}} />
     <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
+    <ProductCard />
+    <ProductCard />
   </Layout>
 );
