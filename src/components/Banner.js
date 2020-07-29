c58038ad59f42b4bbc2a4a0087387fc84dbd8c85
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-07-29 15:59:24 +0900

    created basic banner

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './banner.scss';
+
+const Banner = () => (
+  <div className="banner-box">
+    <div className="banner-image-box">
+    
+    </div>
+    <div className="banner-color-box">
+
+    </div>
+    <div className="banner-title">
+      <h1 className="banner-bold-title">Find <span className="banner-cursive-title">your beauty</span></h1>
+    </div>
+  </div>
+);
+
+export default Banner;
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import Layout from '../components/layout'
-import ProductCard from '../components/ProductCard'
+import React from 'react';
+import Layout from '../components/layout';
+import ProductCard from '../components/ProductCard';
+import Banner from '../components/Banner';
 
 export default ({pageContext}) => (
   <Layout>
+    <Banner />
     <h1 dangerouslySetInnerHTML={{__html: pageContext.title}} />
     <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
     <ProductCard />
