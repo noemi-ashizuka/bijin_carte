3b0c87ea5e97ff6abc20f59db9082d90621d6d7f
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-07-29 22:51:17 +0900

    added fonts and styled cards

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
 import ProductsList from '../components/ProductsList';
-// import ProductCard from '../components/ProductCard';
 import Banner from '../components/Banner';
 
 export default ({pageContext}) => (
