73f720363165769d7d383ff94bfaf01253e0814e
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-01 11:26:56 +0900

    styled banner for small screens

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,15 +3,13 @@ import '../styles/banner.scss';
 
 const Banner = () => (
   <div className="banner-box">
+    <div className="banner-color-box"></div>
     <div className="banner-image-box">
-    
-    </div>
-    <div className="banner-color-box">
-
-    </div>
-    <div className="banner-title">
-      <h1 className="banner-bold-title">Find <span className="banner-cursive-title">your beauty</span></h1>
-    </div>
+        <div className="banner-bg-image"></div>
+        <div className="banner-title">
+          <h1 className="banner-bold-title">Find <br/><span className="banner-cursive-title">your beauty</span></h1>
+        </div>
+      </div>
   </div>
 );
 
