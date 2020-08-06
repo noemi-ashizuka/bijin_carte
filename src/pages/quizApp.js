113f642a1e5736ded904a711408df1cfc35e140b
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-06 16:27:47 +0900

    added all answers to state

--- src/pages/quizApp.js
+++ src/pages/quizApp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { UseState } from 'react';
 import MultipleQuizQuestions from '../data/multipleQuizQuestions';
 import QuestionBox from '../components/QuestionBox';
 import Result from '../components/Result';
@@ -10,10 +10,7 @@ class quizApp extends React.Component {
     this.state= {
       questions: [],
       questionCount: 0,
-      typeA: 0,
-      typeB: 0,
-      typeC: 0,
-      typeD: 0
+      userAnswers: []
     }
   };
 
@@ -23,28 +20,13 @@ class quizApp extends React.Component {
       this.setState({
         questions: question
       });
-      console.log(this.state)
     })
   };
 
   computeAnswer = (answer) => {
-    if(answer === "A") {
-      this.setState({
-        typeA: this.state.typeA + 1
-      })
-    } else if (answer === "B") {
-      this.setState({
-        typeB: this.state.typeB + 1
-      })
-    } else if (answer ==="C") {
-      this.setState({
-        typeC: this.state.typeC + 1
-      })
-    } else {
-      this.setState({
-        typeC: this.state.typeC + 1
-      })
-    }
+    this.setState({
+      userAnswers: [...this.state.userAnswers, answer]
+    });
     this.setState({
       questionCount: this.state.questionCount < 3 ? this.state.questionCount + 1 : 3
     })
