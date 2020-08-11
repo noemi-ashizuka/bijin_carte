3d947e16105795b6a27607430440cd9536277c8a
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-11 14:18:44 +0900

    basic styling for quiz result

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,40 +1,68 @@
 import React from 'react';
 import Button from './Button';
+import { faTwitterSquare, faFacebookSquare} from '@fortawesome/free-brands-svg-icons';
 import {
   FacebookShareButton,
   TwitterShareButton
 } from 'react-share';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../styles/result.scss';
 
 const Result = ({resultType}) => (
   <div>
     {resultType === "A" &&
-      <div>
-        <h1>A: イエベ春</h1>
-        <img src="https://images.unsplash.com/photo-1522748906645-95d8adfd52c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="cherry blossoms" />
-        <p>黄みで明るくクリアな色</p>
+      <div className="quiz-result-container">
+        <h1 className="quiz-result-title">A: イエベ春</h1>
+        <div className="quiz-result-color-box">
+          <img src="https://images.unsplash.com/photo-1522748906645-95d8adfd52c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="cherry blossoms" className="quiz-result-image" />
+          <p className="quiz-result-text">黄みで明るくクリアな色</p>
+        </div>
       </div>
     }
     {resultType === "B" &&
-      <div>
-        <h1>B: イエベ秋</h1>
-        <img src="https://images.unsplash.com/photo-1538580619159-6c19131e1062?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="fall leaves" />
-        <p>黄みで暗くシックな色</p>
-        <FacebookShareButton url="http://google.com" children={<Button title="Share on Facebook" />} quote="B: イエベ秋" hashtag="findyourbeauty" />
-        <TwitterShareButton url="http://google.com" children={<Button title="Share on Twitter" />} title="Bijin Carte" hashtags={["bijincarte", "findyourbeauty"]} />
+      <div className="quiz-result-container">
+        <h1 className="quiz-result-title">B: イエベ秋</h1>
+        <div className="quiz-result-color-box">
+          <img src="https://images.unsplash.com/photo-1538580619159-6c19131e1062?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="fall leaves" className="quiz-result-image" />
+          <p className="quiz-result-text">黄みで暗くシックな色</p>
+        </div>
+        <FacebookShareButton
+          url="http://google.com"
+          children={<FontAwesomeIcon
+            icon={faFacebookSquare}
+            className="quiz-share-icon" 
+            />}
+          quote="B: イエベ秋"
+          hashtag="findyourbeauty"
+        />
+        <TwitterShareButton
+          url="http://google.com"
+          children={<FontAwesomeIcon 
+            icon={faTwitterSquare}
+            className="quiz-share-icon"
+            />} className="quiz-result-title"
+          title="B: イエベ秋"
+          via="bijin_carte"
+          hashtags={["bijincarte", "findyourbeauty"]}
+        />
       </div>
     }
     {resultType === "C" &&
-      <div>
-        <h1>C: ブルベ夏</h1>
-        <img src="https://images.unsplash.com/photo-1534237649404-2cc344128fdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="hydrangea" />
-        <p>青みで明るくソフトな色</p>
+      <div className="quiz-result-container">
+        <h1 className="quiz-result-title">C: ブルベ夏</h1>
+        <div className="quiz-result-color-box">
+          <img src="https://images.unsplash.com/photo-1534237649404-2cc344128fdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="hydrangea" className="quiz-result-image" />
+          <p className="quiz-result-text">青みで明るくソフトな色</p>
+        </div>
       </div>
     }
     {resultType === "D" &&
-      <div>
-        <h1>D: ブルベ冬</h1>
-        <img src="https://images.unsplash.com/photo-1547457604-7d43509866ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="winter berries" />
-        <p>青みで暗い色&ビビットな色</p>
+      <div className="quiz-result-container">
+        <h1 className="quiz-result-title">D: ブルベ冬</h1>
+        <div className="quiz-result-color-box">
+          <img src="https://images.unsplash.com/photo-1547457604-7d43509866ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="winter berries" className="quiz-result-image" />
+          <p className="quiz-result-text">青みで暗い色&ビビットな色</p>
+        </div>
       </div>
     }
 
