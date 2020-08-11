2e94db7872bc76f21579f43fc302174826937761
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-11 16:57:21 +0900

    removed download button from lightbox

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -6,6 +6,12 @@ import { SRLWrapper } from 'simple-react-lightbox';
 import '../styles/gallery.scss';
 import Img from 'gatsby-image';
 
+const options = {
+  buttons: {
+    showDownloadButton: false,
+  }
+}
+
 const Gallery= () => (
   <StaticQuery query={graphql`
   {
@@ -38,7 +44,7 @@ const Gallery= () => (
       <div className="gallery-wrapper">
         <h1 className="gallery-title">Gallery</h1>
         <h3 className="gallery-subtitle">各メニューのサンプルのスライドショー</h3>
-        <SRLWrapper>
+        <SRLWrapper options={options}>
           <div className="gallery-container">
             {props.allWordpressWpMedia.edges.map(image =>
               <Fade left duration={1500} key={image.node.id}>
