f118ddd27ffef7fa0d4923f5254a12680dd46115
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-11 15:47:31 +0900

    fixed margins for titles wrappers

--- src/components/ShindanList.js
+++ src/components/ShindanList.js
@@ -5,7 +5,7 @@ import '../styles/shindanlist.scss';
 const ShindanList = () => (
   <div className="shindan-wrapper">
     <h1 className="shindan-title">Shindan</h1>
-    <h3 className="shindan-subtitle">今のあなたが仕方そのうち不都合で、壇の話まではできるだけ見ずのがえらいとありがは高いまでののでで。</h3>
+    <h3 className="shindan-subtitle">今のあなたが仕方そのうち不都合で、壇の話まではできるだけ見ずのがえらいとありがは高いまでのので。</h3>
     <div className="shindan-container">
       <Link to="/quizApp" className="shindan-thumb">
         <div>
