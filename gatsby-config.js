6523d4c7d6c46f57ebbdee9b3d0895ff15ed1dd9
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-11 10:16:58 +0900

    changed config for local

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -38,9 +42,9 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'demo.wp-api.org' or 'www.example-site.com'
          */
-        baseUrl: "http://bijincarte.local/",
+        baseUrl: process.env.API_URL, //"http://bijincarte.local/",
         // The protocol. This can be http or https.
-        protocol: "http",
+        protocol: process.env.API_PROTOCOL, //"http",
         // The rest api route prefix that your WordPress site is using.
         // Sometimes this is modified by WordPress plugins.
         // If not set, it uses the default of "wp-json"
