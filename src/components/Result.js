7eab1e36d5e7a9de67a2e686e011288b4a4336fd
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-11 11:47:17 +0900

    added basic structure of conditional results

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,8 +2,37 @@ import React from 'react';
 
 const Result = ({resultType}) => (
   <div>
-    This is your result: {resultType}
+    {resultType === "A" &&
+      <div>
+        <h1>A: イエベ春</h1>
+        <img src="https://images.unsplash.com/photo-1522748906645-95d8adfd52c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="cherry blossoms" />
+        <p>黄みで明るくクリアな色</p>
+      </div>
+    }
+    {resultType === "B" &&
+      <div>
+        <h1>B: イエベ秋</h1>
+        <img src="https://images.unsplash.com/photo-1538580619159-6c19131e1062?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="fall leaves" />
+        <p>黄みで暗くシックな色</p>
+      </div>
+    }
+    {resultType === "C" &&
+      <div>
+        <h1>C: ブルベ夏</h1>
+        <img src="https://images.unsplash.com/photo-1534237649404-2cc344128fdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="hydrangea" />
+        <p>青みで明るくソフトな色</p>
+      </div>
+    }
+    {resultType === "D" &&
+      <div>
+        <h1>D: ブルベ冬</h1>
+        <img src="https://images.unsplash.com/photo-1547457604-7d43509866ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="winter berries" />
+        <p>青みで暗い色&ビビットな色</p>
+      </div>
+    }
+
   </div>
+  
 )
 
 export default Result;
