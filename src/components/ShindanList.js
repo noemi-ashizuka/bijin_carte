2949c045a7bd67e0f558e65352484059af8361d7
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-11 17:10:03 +0900

    added fade effect

--- src/components/ShindanList.js
+++ src/components/ShindanList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import {Fade} from 'react-reveal';
 import '../styles/shindanlist.scss';
 
 const ShindanList = () => (
@@ -7,21 +8,23 @@ const ShindanList = () => (
     <h1 className="shindan-title">Shindan</h1>
     <h3 className="shindan-subtitle">今のあなたが仕方そのうち不都合で、壇の話まではできるだけ見ずのがえらいとありがは高いまでのので。</h3>
     <div className="shindan-container">
-      <Link to="/quizApp" className="shindan-thumb">
-        <div>
-          Personal Color
-        </div>
-      </Link>
-      <Link to="/quizApp" className="shindan-thumb">
-        <div>
-          Coming Soon
-        </div>
-      </Link>
-      <Link to="/quizApp" className="shindan-thumb">
-        <div>
-          Coming Soon
-        </div>
-      </Link>
+      <Fade right duration={1500}>
+        <Link to="/quizApp" className="shindan-thumb">
+          <div>
+            Personal Color
+          </div>
+        </Link>
+        <Link to="/quizApp" className="shindan-thumb">
+          <div>
+            Coming Soon
+          </div>
+        </Link>
+        <Link to="/quizApp" className="shindan-thumb">
+          <div>
+            Coming Soon
+          </div>
+        </Link>
+      </Fade>
     </div>
   </div>
 )
