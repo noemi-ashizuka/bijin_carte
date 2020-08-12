a12d41dc8e7e43c5f8935c47c4e71df72c37622d
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-12 14:32:14 +0900

    changed default site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,10 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bijin Carte`,
+    description: `Find your beauty with bijin carte, personalized make up lessons`,
+    url: `https://www.noemi-ashizuka.com`, // image ?
+    author: `BijinCarte`,
   },
   plugins: [
     `gatsby-plugin-sass`,
