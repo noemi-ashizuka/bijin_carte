cbe0d32b642a24364e708c737e70baeb58393464
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-13 14:29:49 +0900

    restyled form radio buttons and label

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -66,59 +66,59 @@ export default class Contact extends React.Component {
         </Form.Group>
 
         <Form.Group className="form-field">
-          <Form.Text className="form-label form-paragraph">Choose an option</Form.Text>
-        </Form.Group>
+          <Form.Label className="form-label">ご希望の内容</Form.Label>
         
-        {['radio'].map((type) =>(
-          <div key={`inline-${type}`} className="mb-3">
+          {['radio'].map((type) =>(
+            <div key={`inline-${type}`} className="mb-3">
 
-            <Form.Check 
-              inline label="メイク体験" 
-              type={type} 
-              id={`inline-${type}-1`} 
-              name="option" 
-              value="value A"
-              checked={this.state.option === "value A"}
-              onChange={this.handleOptionChange}
-              className="form-radio" />
-            <Form.Check 
-              inline label="オンラインメイクレッスン" 
-              type={type} 
-              id={`inline-${type}-2`} 
-              name="option" 
-              value="value B"
-              checked={this.state.option === "value B"}
-              onChange={this.handleOptionChange}
-              className="form-radio" />
-            <Form.Check 
-              inline label="美人カルテ" 
-              type={type} 
-              id={`inline-${type}-3`} 
-              name="option" 
-              value="value C"
-              checked={this.state.option === "value C"}
-              onChange={this.handleOptionChange}
-              className="form-radio" />
               <Form.Check 
-              inline label="ポーチのお悩み解決します" 
-              type={type} 
-              id={`inline-${type}-4`} 
-              name="option" 
-              value="value D"
-              checked={this.state.option === "value D"}
-              onChange={this.handleOptionChange}
-              className="form-radio" />
+                inline label="メイク体験" 
+                type={type} 
+                id={`inline-${type}-1`} 
+                name="option" 
+                value="value A"
+                checked={this.state.option === "value A"}
+                onChange={this.handleOptionChange}
+                className="form-radio" />
               <Form.Check 
-              inline label="その他" 
-              type={type} 
-              id={`inline-${type}-5`} 
-              name="option" 
-              value="value E"
-              checked={this.state.option === "value E"}
-              onChange={this.handleOptionChange}
-              className="form-radio" />
-          </div>
-        ))}
+                inline label="オンラインメイクレッスン" 
+                type={type} 
+                id={`inline-${type}-2`} 
+                name="option" 
+                value="value B"
+                checked={this.state.option === "value B"}
+                onChange={this.handleOptionChange}
+                className="form-radio" />
+              <Form.Check 
+                inline label="美人カルテ" 
+                type={type} 
+                id={`inline-${type}-3`} 
+                name="option" 
+                value="value C"
+                checked={this.state.option === "value C"}
+                onChange={this.handleOptionChange}
+                className="form-radio" />
+                <Form.Check 
+                inline label="ポーチのお悩み解決します" 
+                type={type} 
+                id={`inline-${type}-4`} 
+                name="option" 
+                value="value D"
+                checked={this.state.option === "value D"}
+                onChange={this.handleOptionChange}
+                className="form-radio" />
+                <Form.Check 
+                inline label="その他" 
+                type={type} 
+                id={`inline-${type}-5`} 
+                name="option" 
+                value="value E"
+                checked={this.state.option === "value E"}
+                onChange={this.handleOptionChange}
+                className="form-radio" />
+            </div>
+          ))}
+        </Form.Group>
 
         <Form.Group className="form-field">
           <Form.Label className="form-label">本文</Form.Label>
