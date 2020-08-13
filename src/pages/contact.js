b5ed1a4c647f662be100027024eadd821a6492c5
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-13 14:19:45 +0900

    added form description

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -41,7 +41,8 @@ export default class Contact extends React.Component {
   render() {
     return <Layout>
       <h1 className="form-title">Contact Us</h1>
-      <h3 className="form-subtitle">ちょっとお仕事の連れようでしょ運動もできやっならて、どんなのからお主人議会からありまし。</h3>
+      <h3 className="form-subtitle">あなたの綺麗を見つけるお手伝い</h3>
+      <p className="form-subtitle">ご予約はもちろん、レッスン内容などに関するお問い合わせも、お気軽にお問い合わせください。取材や撮影のご依頼もこちらからお願いいたします。</p>
       <Form onSubmit={this.handleSubmit} className="form-wrapper">
         
         <Form.Group className="form-field">
