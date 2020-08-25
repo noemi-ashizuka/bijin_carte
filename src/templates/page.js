0b4161c4f1488b0adcedd5515865f6777ac5c1f3
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-08-25 11:32:49 +0900

    refactored

--- src/templates/page.js
+++ src/templates/page.js
@@ -4,22 +4,6 @@ import ReactHtmlParser from 'react-html-parser';
 import { Helmet } from "react-helmet";
 import '../styles/pagetemplate.scss';
 
-// function openQuestion() {
-//   let answers = document.querySelectorAll('.answer-paragraph');
-//   let questions = document.querySelectorAll('.question-trigger')
-//     questions.forEach(question => 
-//       question.addEventListener('click', (event) => {
-//         // answers.forEach(answer => {
-//         //   answer.classList.toggle('answer-hidden')
-//         console.log(event.target)
-//         })
-//       )
-    
-
-  
-  
-// }
-
 export default ({pageContext}) => (
   <Layout>
     <Helmet>
