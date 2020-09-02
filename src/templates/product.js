9f8e81c17b653d5eb5986f038ff51fb931fa003d
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-09-02 14:43:59 +0900

    adding colored layer to product photo

--- src/templates/product.js
+++ src/templates/product.js
@@ -7,20 +7,23 @@ import '../styles/productdetails.scss';
 
 export default ({pageContext}) => (
   <Layout>
-    <Helmet>
-      { ReactHtmlParser(pageContext.yoast_head)}
-    </Helmet>
-    <div className="product-details-wrapper">
-      <h1 className="product-details-title">{pageContext.title}</h1>
-      {pageContext.featured_media &&
+  <Helmet>
+    { ReactHtmlParser(pageContext.yoast_head)}
+  </Helmet>
+  <div className="product-details-wrapper">
+    <h1 className="product-details-title">{pageContext.title}</h1>
+    {pageContext.featured_media &&
+      <div className="product-image-box">
+        <div className="product-image-color-box"></div>
         <img src={pageContext.featured_media.source_url} alt={pageContext.featured_media.alt_text} className="product-details-image" />
-      }
-      <div className="product-details-color-box">
-        <div dangerouslySetInnerHTML={{__html: pageContext.content}} className="product-details-text" />
       </div>
-    </div>
-    {pageContext.acf.image &&
-      <GalleryPost imgOne={pageContext.acf.image} imgTwo={pageContext.acf.image_2} imgThree={pageContext.acf.image_3} />
     }
-  </Layout>
+    <div className="product-details-color-box">
+      <div dangerouslySetInnerHTML={{__html: pageContext.content}} className="product-details-text" />
+    </div>
+  </div>
+  {pageContext.acf.image &&
+    <GalleryPost imgOne={pageContext.acf.image} imgTwo={pageContext.acf.image_2} imgThree={pageContext.acf.image_3} />
+  }
+</Layout>
 );
